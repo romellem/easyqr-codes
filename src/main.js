529e5031fd8a319792915d19d232db6f92dcc29a
romellem <romellem.git@gmail.com>
2021-02-27 16:36:17 -0600

    Initializes with a QR Code

--- src/main.js
+++ src/main.js
@@ -10,26 +10,26 @@ const $ = (selector, ele) => (ele ? ele.querySelector(selector) : document.query
 const svg = $('#qrcode-svg');
 const textarea_input = $('#textarea-input');
 
-textarea_input.oninput = redrawQrCode;
+textarea_input.oninput = function onTextAreaChange(e) {
+	redrawQrCode(textarea_input.value);
+};
 
-function redrawQrCode() {
+function redrawQrCode(text) {
 	// Reset output images in case of early termination
-
 	svg.style.display = 'none';
 
 	// Get form inputs and compute QR Code
 	const ecl = QrCode.Ecc.LOW;
-	const text = textarea_input.value;
 	const segs = QrSegment.makeSegments(text);
 	const minVer = 1;
 	const maxVer = 40;
 	const mask = -1;
 	const boostEcc = true;
 	const qr = QrCode.encodeSegments(segs, ecl, minVer, maxVer, mask, boostEcc);
-	// Draw image output
 	const border = 0;
 
 	const code = qr.toSvgString(border);
+
 	const viewBox = / viewBox="([^"]*)"/.exec(code)[1];
 	const pathD = / d="([^"]*)"/.exec(code)[1];
 	svg.setAttribute('viewBox', viewBox);
@@ -37,4 +37,4 @@ function redrawQrCode() {
 	svg.style.display = '';
 }
 
-setTimeout(() => redrawQrCode(), 100);
+redrawQrCode('https://easyqr.codes/');
